799c1454e39b9f6dc920d1f390edade5c0912a8a
djalil0 <112862859+djalil0@users.noreply.github.com>
2026-04-02 01:41:13 +0300

    add a sorting strategy

--- src/main/java/ru/aston/hometask/finalproject/services/UserService.java
+++ src/main/java/ru/aston/hometask/finalproject/services/UserService.java
@@ -1,13 +1,20 @@
-package ru.aston.hometask.finalproject.services;
+package main.java.ru.aston.hometask.finalproject.services;
 
+import main.java.ru.aston.hometask.finalproject.services.sorting.Sort;
 import ru.aston.hometask.finalproject.models.User;
 
 import java.util.List;
 
 public class UserService {
     // TODO: Реализовать сортировку списка пользователей
-
+    private Sort sortStrategy;
+    public void setSortStrategy(Sort sortStrategy) {
+        this.sortStrategy = sortStrategy;
+    }
     public void sort(final List<User> users) {
-
+        if (sortStrategy == null) {
+            throw new IllegalStateException("Sort strategy not set");
+        }
+        sortStrategy.sort(users);
     }
 }
