8ede3c62bf650e75d53b463746bfd82a9521429e
defaultxod <defaultx-od@yandex.ru>
2026-04-03 09:16:57 +0300

    Исправил импорты в сервисе, удалил пакет sorting в пакете services

--- src/main/java/ru/aston/hometask/finalproject/services/UserService.java
+++ src/main/java/ru/aston/hometask/finalproject/services/UserService.java
@@ -1,20 +1,6 @@
-package main.java.ru.aston.hometask.finalproject.services;
-
-import main.java.ru.aston.hometask.finalproject.services.sorting.Sort;
-import ru.aston.hometask.finalproject.models.User;
-
-import java.util.List;
+package ru.aston.hometask.finalproject.services;
 
 public class UserService {
     // TODO: Реализовать сортировку списка пользователей
-    private Sort sortStrategy;
-    public void setSortStrategy(Sort sortStrategy) {
-        this.sortStrategy = sortStrategy;
-    }
-    public void sort(final List<User> users) {
-        if (sortStrategy == null) {
-            throw new IllegalStateException("Sort strategy not set");
-        }
-        sortStrategy.sort(users);//kk
-    }
+
 }
