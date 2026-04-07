e2312eb5212fb967c7926bd36bfb84b116fe8157
fiash-dev <fiash099rnd@gmail.com>
2026-04-08 01:35:38 +0300

    - поправил

--- src/main/java/ru/aston/hometask/finalproject/filesystem/FileReader.java
+++ src/main/java/ru/aston/hometask/finalproject/filesystem/FileReader.java
@@ -2,14 +2,21 @@ package ru.aston.hometask.finalproject.filesystem;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileReader {
 
     public boolean isFileExists(final String userPath) {
-        final Path filePath = Paths.get(userPath);
-        return Files.exists(filePath);
+        try {
+            final Path filePath = Paths.get(userPath);
+            return Files.exists(filePath);
+        } catch (InvalidPathException e) {
+            System.err.println("Недопустимый формат пути: " + userPath);
+            System.err.println("Ошибка: " + e.getMessage());
+            return false;
+        }
     }
 
     public String readFile(final String userPath) {
