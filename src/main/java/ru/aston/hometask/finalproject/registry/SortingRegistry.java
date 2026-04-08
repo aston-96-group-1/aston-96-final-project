0583374110c76d06a39d97bb6c4845e4d4fae929
lizavet-byte <lizavetarussell@gmail.com>
2026-04-08 21:19:20 +0500

    добавлен SortingRegistry

---
+++ src/main/java/ru/aston/hometask/finalproject/registry/SortingRegistry.java
@@ -0,0 +1,23 @@
+package ru.aston.hometask.finalproject.registry;
+
+import ru.aston.hometask.finalproject.sorting.Sort;
+
+import java.util.List;
+import java.util.Map;
+
+public class SortingRegistry {
+
+    private Map<String, Sort> sorters;
+
+    public SortingRegistry(final Map<String, Sort> sorters) {
+        this.sorters = sorters;
+    }
+
+    public Sort getProviderByKey(final String key) {
+        return sorters.get(key);
+    }
+
+    public List<String> getKeys() {
+        return sorters.keySet().stream().toList();
+    }
+}
