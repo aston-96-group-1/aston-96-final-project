904f2f1663f9b5e3b72a57722744c15ca7594ba9
lizavet-byte <lizavetarussell@gmail.com>
2026-04-08 21:34:36 +0500

    Добавила зависимость для ConsoleUI

--- src/main/java/ru/aston/hometask/finalproject/Main.java
+++ src/main/java/ru/aston/hometask/finalproject/Main.java
@@ -9,10 +9,16 @@ import ru.aston.hometask.finalproject.providers.FileUserProvider;
 import ru.aston.hometask.finalproject.providers.IUserProvider;
 import ru.aston.hometask.finalproject.providers.ManualUserProvider;
 import ru.aston.hometask.finalproject.providers.RandomUserProvider;
+import ru.aston.hometask.finalproject.registry.SortingRegistry;
 import ru.aston.hometask.finalproject.registry.UserProviderRegistry;
 import ru.aston.hometask.finalproject.services.UserService;
+import ru.aston.hometask.finalproject.sorting.SortByEmail;
+import ru.aston.hometask.finalproject.sorting.SortByName;
+import ru.aston.hometask.finalproject.sorting.SortByPostCount;
+import ru.aston.hometask.finalproject.sorting.SortByPostCountEvenOnly;
 import ru.aston.hometask.finalproject.ui.ConsoleUI;
 import ru.aston.hometask.finalproject.validation.Validator;
+import ru.aston.hometask.finalproject.sorting.Sort;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -34,10 +40,19 @@ public class Main {
         providerMap.put("2", new RandomUserProvider(validator, random));
         providerMap.put("3", new FileUserProvider(scanner, validator, fileReader, gson));
 
+        final Map<String, Sort> sortingMap = new LinkedHashMap<>();
+        sortingMap.put("1", new SortByName());
+        sortingMap.put("2", new SortByEmail());
+        sortingMap.put("3", new SortByPostCount());
+        sortingMap.put("4", new SortByPostCountEvenOnly());
+
         final UserProviderRegistry userProviderRegistry = new UserProviderRegistry(providerMap);
         final UserService userService = new UserService();
 
-        final ConsoleUI cli = new ConsoleUI(userProviderRegistry, userService, scanner, fileWriter, dotenv);
+        final SortingRegistry sortingRegistry = new SortingRegistry(sortingMap);
+
+        final ConsoleUI cli = new ConsoleUI(userProviderRegistry, userService, scanner, fileWriter, dotenv,
+                sortingRegistry);
 
         cli.launch();
     }
